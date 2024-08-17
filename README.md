43769019c3d39c67c1fd77059c011206c2aba800
tripple6design <bill.miller@tripple6.design>
2024-08-16 22:12:31 -0500

    Initial Commit
    This is the initial push of the code to the repository.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MAK Styler
- This Webtoy will style things
+ 
+ This is a webtoy which will remove inline style tas for any HTML elements pasted into it.
+
+ ## How it works
+
+ When you paste HTML into the textarea textbox the tool will automatically load it into a Document Fragment, perform a querySelectorAll query on the document fragment for anything with a style attribute.  It will then use the Element.removeAttribute function to remove them from each of the elements in the NodeList.  When this procedure is completed, the HTML from the document fragment will then be displayed on the screen for copy-paste.
---
+++ code/scripts.js
@@ -0,0 +1,49 @@
+const inputMarkup=document.querySelectorAll("textarea")[0],
+outputMarkup=document.querySelectorAll("textarea")[1],
+output=document.querySelector("output"),
+EMPTY="",
+fragment=document.createElement("article");
+
+inputMarkup.addEventListener("input", process);
+
+outputMarkup.is=function(string){this.value=string;}
+outputMarkup.clear=function(){this.is(EMPTY);}
+
+output.is=function(string){this.innerHTML=string;}
+output.clear=function() {this.is(EMPTY);}
+
+fragment.is=function(string) {this.innerHTML=string;}
+fragment.clear=function() {fragment.is(EMPTY);}
+
+fragment.remove=function(attribute) {
+    this.is(inputMarkup.value);
+
+    elements=this.querySelectorAll(`[${attribute}]`);
+
+    if(elements.length>0) {
+        for(let index=0;index<elements.length;index++) {
+            elements[index].removeAttribute(attribute);
+        }
+    }
+}
+
+function process() {
+    outputMarkup.clear();
+    output.clear();
+
+    if(inputMarkup.value.length>0) {
+
+        fragment.clear();
+        fragment.is(inputMarkup.value);
+        fragment.remove("style");
+
+        output.clear();
+        outputMarkup.clear();
+
+        output.is(fragment.innerHTML);
+        outputMarkup.is(fragment.innerHTML);
+
+    }
+
+    return;
+}
